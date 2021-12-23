f8b86d43c2ce8bf9dcfd3fa7aa2d0a60dd1568d3
Büşra Yeşilalioğlu <88002831+7Auri@users.noreply.github.com>
2021-12-23 18:05:12 +0300

    Create mario_advanced.c

---
+++ mario_advanced.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+     int sayi= get_int("Yükseklik Giriniz:\n");
+  if(sayi<9){
+      for (int i = 0; i < sayi; i++) { 
+          for (int j = sayi-i; j >1; j--) { 
+              
+               printf(" ");}
+          for(int k=0;k<i+1;k++){
+              printf("#"); }
+              printf(" ");
+              printf(" ");
+             for(int e=0;e<i+1;e++){
+                   printf("#");
+             }
+              
+               printf("\n"); 
+  
+ }}}
